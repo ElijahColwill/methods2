4073b52f56924d8c5e168df3b0f5c44760781ab2
EljiahColwill <elijah.c035@gmail.com>
2017-09-28 12:09:55 -0600

    Implement ice_cream_party?

--- methods2.rb
+++ methods2.rb
@@ -23,6 +23,16 @@ end
 	
 # TODO - write ice_cream_party?
 
+def ice_cream_party?(ice_cream, candy)
+	if (ice_cream < 5) || (candy < 5)
+		return 0
+	end
+	if (ice_cream >= candy * 2) || (candy >= ice_cream * 2)
+		return 2
+	end
+	return 1
+end
+
 # TODO - write successful_squirrel_party?
 
 # TODO - write ticket
